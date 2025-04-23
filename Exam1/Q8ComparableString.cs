7051c7ef556b62f6a95854d2429dba12ad9f0152
najme <najmesalmanpour@gmail.com>
2025-04-23 10:21:27 +0330

    exam1

---
+++ Exam1/Q8ComparableString.cs
@@ -0,0 +1,12 @@
+namespace Exam1;
+
+ public class Q8StringLengthComparer //: IComparer<string>
+ {
+//     public string? STR { get; set; }
+
+//     public bool Compare(string? x, string? y)
+//     {
+
+//     }
+ }
+
